4d8887deca4bbb21cb2277777db1b748a1158a5f
denyosaur <danielkim1989@gmail.com>
2023-08-01 16:52:43 -0700

    ordering date correctly for graphs

--- components/GraphSection/index.tsx
+++ components/GraphSection/index.tsx
@@ -10,16 +10,18 @@ const GraphSection: React.FC<GraphSectionProps> = async ({ heroId }) => {
     }
   })
 
+  const sortedData = res.sort((a, b) => Number(a.day) - Number(b.day));
+
   return (
     <div className="flex flex-col justify-between h-full w-full">
       <div className="flex flex-col justify-around items-center h-full">
         <div className="w-[38rem]">
           <h3 className="text-center py-1">Win Count</h3>
-          <WinCountGraph data={res} />
+          <WinCountGraph data={sortedData} />
         </div>
         <div className="w-[38rem]">
           <h3 className="text-center py-1">Win Rate</h3>
-          <WinRateGraph data={res} />
+          <WinRateGraph data={sortedData} />
         </div>
       </div>
     </div>
